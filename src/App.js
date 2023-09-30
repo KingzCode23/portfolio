1a68babc9ba6d4f8b2b4f348e08bff713dce6a96
king <kingbraynare@gmail.com>
2023-09-30 11:39:59 -0700

    header-save

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import Header from './components/header/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header />
     </div>
   );
 }
