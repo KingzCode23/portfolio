6374c45eeaa37ac38b008ec6009c29434617515a
king <kingbraynare@gmail.com>
2023-10-03 09:02:07 -0700

    testimonial

--- src/components/certificate/Certificate.jsx
+++ src/components/certificate/Certificate.jsx
@@ -1,11 +1,53 @@
 import React from 'react'
 import './certificate.css'
+import { Data } from './Data'
+import { Swiper, SwiperSlide } from 'swiper/react';
 
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+// import required modules
+import { Pagination } from 'swiper/modules';
 
 
 const Certificate = () => {
   return (
-    <div>Certificate</div>
+    <section className="testimonial container section">
+      <h2 className="section__title">My Certificate</h2>
+      <span className="section__subtitle">Certificate</span>
+      <Swiper className="testimonail__container"
+      
+      loop={true}
+      grabCursor={true }
+      spaceBetween={24}
+      pagination={{
+        clickable: true,
+      }}
+      breakpoints={{
+        576: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 48,
+        },
+      }}
+      modules={[Pagination]}
+      
+      >
+        {Data.map(({id, image, title, description}) => {
+          return (
+            <SwiperSlide className='testimonial__card' key={id}>
+               <img src={image} alt='img' className='testimonial__img' />
+               <h3 className="testimonial__name">{title}</h3>
+               <p className="testimonial__description">{description}</p>
+            </SwiperSlide>
+          )
+        })}
+      </Swiper>
+    </section>
   )
 }
 
---
+++ src/components/certificate/Data.jsx
@@ -0,0 +1,24 @@
+import Are from '../../assets/Are.jpg'
+import Host from '../../assets/host.jpg'
+import Hosts from '../../assets/Are.jpg'
+
+
+export const Data = [{
+    id: 1,
+    image: Are,
+    title: '6 ITEE Bootcamp 2021',
+    description: 'Successfully Completed the Unboxing Opportunities for Inovation in the Pandemic'
+},
+{
+    id: 2,
+    image: Host,
+    title: 'ITEE Bootcamp',
+    description: 'Successfully Completed the 3-days Information Technology Electronics Engineering Training'
+},
+{
+    id: 3,
+    image: Hosts,
+    title: '6 ITEE Bootcamp 2021',
+    description: 'Successfully Completed the Unboxing Opportunities for Inovation in the Pandemic'
+}
+]
